e75281acad463e10175859b5e0a4ea03022fa8af
MaxxinHell <150080754+MaxxinHell@users.noreply.github.com>
2024-02-23 12:22:25 -0600

    add file

---
+++ Calificacion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+    int cal;
+    scanf("%d" , &cal);
+    if (cal >= 8)
+    {
+        printf("Felicidades aprobaste el curso");
+    }
+    printf("Fin del programa");
+
+    return 0;
+}
